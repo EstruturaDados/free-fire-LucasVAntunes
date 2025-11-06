26a6c88b75ef491b5a278793f2b699d007228cdb
LucasVAntunes <lucasvieira001@gmail.com>
2025-11-06 20:22:27 +0000

    firstcommit

--- FreeFire.c
+++ FreeFire.c
@@ -8,6 +8,19 @@
 // Este programa simula o gerenciamento avançado de uma mochila com componentes coletados durante a fuga de uma ilha.
 // Ele introduz ordenação com critérios e busca binária para otimizar a gestão dos recursos.
 
+#define MAX_STR_LEN 50
+#define TAM_MAX 10
+
+typedef struct {
+    char itens [TAM_MAX][MAX_STR_LEN];
+    int numitens;
+} inventario;
+
+void inicializar_mochila(inventario *slot);
+void inserir_item (inventario *slot, const char* novoitem);
+void remover_item (inventario *slot, const char* novoitem);
+void listar_item (const inventario *slot);
+
 int main() {
     // Menu principal com opções:
     // 1. Adicionar um item
@@ -17,11 +30,61 @@ int main() {
     // 5. Realizar busca binária por nome
     // 0. Sair
 
+    int opcao;
+
+    inventario mochila;
+    char item_nome[MAX_STR_LEN];
+    inicializar_mochila(&mochila);
+
+    printf ("INVENTÁRIO\n\n");
+
+    do
+    {
+        printf ("1. Adicionar um item\n");
+        printf ("2. Remover um item\n");
+        printf ("3. Listar todos os itens\n");
+        printf ("0. Sair\n");
+        scanf ("%d", &opcao);
+
     // A estrutura switch trata cada opção chamando a função correspondente.
     // A ordenação e busca binária exigem que os dados estejam bem organizados.
+    
+        switch (opcao)
+        {
+        case 1:
+            printf("Digite o nome do item para adicionar: ");
+            scanf(" %[^\n]", item_nome);
+            inserir_item(&mochila, item_nome);
+            break;
+        case 2:
+            printf("Digite o nome do item para remover: ");
+            scanf(" %[^\n]", item_nome);
+            remover_item(&mochila, item_nome);
+            break;
+        case 3:
+            listar_item(&mochila);
+            break;
+        case 0:
+            printf("Saindo...\n");
+            break;
+        
+        default:
+        
+        printf("Opção inválida!\n");
+        
+            break;
+        }
+
+    } while (opcao != 0);
+    
+
+
 
     return 0;
 }
+
+
+
 
 // Struct Item:
 // Representa um componente com nome, tipo, quantidade e prioridade (1 a 5).
@@ -34,6 +97,10 @@ int main() {
 // Armazena até 10 itens coletados.
 // Variáveis de controle: numItens (quantidade atual), comparacoes (análise de desempenho), ordenadaPorNome (para controle da busca binária).
 
+void inicializar_mochila(inventario *slot) {
+    slot -> numitens = 0;
+}
+
 // limparTela():
 // Simula a limpeza da tela imprimindo várias linhas em branco.
 
@@ -44,13 +111,53 @@ int main() {
 // Adiciona um novo componente à mochila se houver espaço.
 // Solicita nome, tipo, quantidade e prioridade.
 // Após inserir, marca a mochila como "não ordenada por nome".
+void inserir_item (inventario *slot, const char* novoitem){
+    if (slot->numitens==TAM_MAX){
+        printf("ERRO: Lista cheia, não é possível inserir.\n");
+        return;
+    }
 
+    strcpy (slot->itens[slot->numitens], novoitem);
+    slot->numitens ++;
+    printf ("Item \"%s\" inserido com sucesso.\n", novoitem);
+}
 // removerItem():
 // Permite remover um componente da mochila pelo nome.
 // Se encontrado, reorganiza o vetor para preencher a lacuna.
+void remover_item (inventario *slot, const char* novoitem){
+    int i, pos = -1;
+    for (i = 0; i < slot->numitens; i++){
+        if (strcmp(slot -> itens[i], novoitem) == 0){
+        pos = i;
+        break;
+        }
+    }
+    if (pos == -1){
+        printf ("Erro: item \"%s\" não encontrado.\n", novoitem);
+        return;
+    }
+    for (i = pos; i < slot->numitens -1; i++){
+        strcpy (slot->itens[i], slot ->itens[i+1]);
+    }
+    slot->numitens--;
+    printf("Item \"%s\" removido com sucesso.\n", novoitem);
+}
 
 // listarItens():
 // Exibe uma tabela formatada com todos os componentes presentes na mochila.
+
+void listar_item (const inventario *slot){
+    if (slot -> numitens == 0){
+        printf("O inventário está vazio.\n");
+        return;
+    }
+    printf ("===========================================\n");
+    printf ("\nINVENTÁRIO\n\n");
+    for(int i = 0; i < slot -> numitens; i++){
+        printf("%s\n", slot ->itens[i]);
+    }
+    printf("\n");
+}
 
 // menuDeOrdenacao():
 // Permite ao jogador escolher como deseja ordenar os itens.
